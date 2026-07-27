a50feca96cb3aed1e72392d6dc07c1bbe997aecd
Michael Kornilich <molego16@gmail.com>
2026-07-27 15:11:16 +0200

    Continue writing dropping logic

--- src/db.py
+++ src/db.py
@@ -199,46 +199,58 @@ def _prepare_drop(id_: int) -> sqlite3.Connection:
 
 
 def _build_where_restrictions(
-        id_: int,
-        name: str | None = None,
-        description_contains: str | None = None,
+        id_: bool = False,
+        name: bool = False,
+        description_contains: bool = False,
         date_created: DateInterval | None = None,
         date_added: DateInterval | None = None,
-        tags: Sequence[str] = (),
+        tags: bool = False,
 ) -> str:
-    """Helper function to build restriction for the WHERE clause"""
-    # TODO: think about SQL injections here
+    """
+    Helper function to build restriction for the WHERE clause.
+    Returns an SQL-string ready for parameter insertion.
+    Parameter names are:
 
-    where_restrictions = [f"id = {id_}"]
+    - (function parameter => SQL parameter)
+    - id_ => id_
+    - name => name
+    - description_contains => description_contains
+    - date_created => date_created_lower, date_created_upper
+    - date_added => date_added_lower, date_added_upper
+    - tags => tags
+    """
+
+    where_restrictions = []
+    if id_:
+        where_restrictions.append(f"id = :id_")
     if name:
-        where_restrictions.append(f"name = {name}")
+        where_restrictions.append(f"name = :name")
 
     if description_contains:
-        where_restrictions.append(f"description LIKE  %{description_contains}%")
+        where_restrictions.append(f"description LIKE :description_contains")
 
     if date_created:
-        lower_sign = ">" + "=" if date_created.include_lower else ""
-        upper_sign = "<" + "=" if date_created.include_upper else ""
+        lower_sign = ">" + ("=" if date_created.include_lower else "")
+        upper_sign = "<" + ("=" if date_created.include_upper else "")
 
-        where_restrictions.append(f"date_created {lower_sign} '{date_created.lower.isoformat()}'")
-        where_restrictions.append(f"date_created {upper_sign} '{date_created.upper.isoformat()}'")
+        where_restrictions.append(f"date_created {lower_sign} :date_created_lower")
+        where_restrictions.append(f"date_created {upper_sign} :date_created_upper")
 
     if date_added:
-        lower_sign = ">" + "=" if date_added.include_lower else ""
-        upper_sign = "<" + "=" if date_added.include_upper else ""
+        lower_sign = ">" + ("=" if date_added.include_lower else "")
+        upper_sign = "<" + ("=" if date_added.include_upper else "")
 
-        where_restrictions.append(f"date_created {lower_sign} '{date_added.lower.isoformat()}'")
-        where_restrictions.append(f"date_created {upper_sign} '{date_added.upper.isoformat()}'")
+        where_restrictions.append(f"date_added {lower_sign} :date_added_lower")
+        where_restrictions.append(f"date_added {upper_sign} :date_added_upper")
 
     if tags:
-        wrapped_tags = [f"'{tag}'" for tag in tags]
-        where_restrictions.append(f"tag in ({', '.join(wrapped_tags)})")
+        where_restrictions.append(f"tag in :tags")
 
-    return ", and ".join(where_restrictions)
+    return " AND ".join(where_restrictions)
 
 
 def drop_file_set(
-        id_: int,
+        id_: int | None = None,
         name: str | None = None,
         description_contains: str | None = None,
         date_created: DateInterval | None = None,
@@ -247,16 +259,19 @@ def drop_file_set(
         dry_run: bool = True,
 ) -> None | int:
     """
-    Drop file that match ALL the specified parameters.
-    That is, if multiple files match the specified parameters, many are dropped.
+    Drop file(s) that match the union (AND) of the specified restrictions.
+    If multiple files match the set of restrictions, all matching are dropped.
+    Unspecified restrictions (None) are ignored.
+
     dry_run: If true, do not drop any files, but return the number of potentially dropped files.
     """
     DB_PATH, STORAGE_PATH = _get_db_vars()
 
-    where_restrictions = _build_where_restrictions(id_, name, description_contains, date_created, date_added, tags)
+    where_restrictions = _build_where_restrictions(bool(id_), bool(name), bool(description_contains),
+                                                   date_created, date_added, bool(tags))
 
     with sqlite3.connect(DB_PATH) as con:
-        res = con.execute(f"""
+        select_query = f"""
         SELECT
             distinct 
             id,
@@ -264,7 +279,24 @@ def drop_file_set(
         FROM "index" i LEFT JOIN "tags" t USING (id)
         WHERE 
             {where_restrictions}
-        """)
+        """
+        params = locals().copy()
+        params.pop("dry_run")
+
+        if params.get("date_created"):
+            params.update({
+                "date_created_lower": params["date_created"].lower,
+                "date_created_upper": params["date_created"].upper,
+            })
+            params.pop("date_created")
+        if params.get("date_added"):
+            params.update({
+                "date_added_lower": params["date_added"].lower,
+                "date_added_upper": params["date_added"].upper,
+            })
+            params.pop("date_added")
+
+        res = con.execute(select_query, params)
         files_to_drop = res.fetchall()
 
     if dry_run:
