21eb402f367042655a5c05500b4fdbceea0d9441
Michael Kornilich <molego16@gmail.com>
2026-08-16 22:17:41 +0200

    Add dev install script

--- scripts/dev-install.py
+++ scripts/dev-install.py
@@ -12,9 +12,9 @@ dev_local_config = {
     "db-path": f"{os.environ['PWD']}/volume/index/index.db",
     "storage-path": f"{os.environ['PWD']}/volume/storage"
 }
-with open("config/local.json", "r") as f:
-    json.dump(dev_local_config, f)
+with open("config/local.json", "w") as f:
+    json.dump(dev_local_config, f, indent=2)
 
 user_local_config = {"landing-directory": f"{os.environ['PWD']}/temp"}
-with open("config/user.json", "r") as f:
-    json.dump(user_local_config, f)
+with open("config/user.json", "w") as f:
+    json.dump(user_local_config, f, indent=1)
